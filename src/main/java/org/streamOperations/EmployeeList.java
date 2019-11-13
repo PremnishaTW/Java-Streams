ff02c40f0d3243095e72310ced092f2f22507508
PremnishaTW <premniba@thoughtworks.com>
2019-11-13 12:36:09 +0530

    Remove impl for demo

--- src/main/java/org/streamOperations/EmployeeList.java
+++ src/main/java/org/streamOperations/EmployeeList.java
@@ -2,7 +2,6 @@ package org.streamOperations;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class EmployeeList {
     private List<Employee> employeeList;
@@ -17,7 +16,7 @@ public class EmployeeList {
      * @return list of employees for a given role
      */
     public List<Employee> getAllEmployeesForRole(EmployeeRole role) {
-        return employeeList.stream().filter(employee -> employee.getRole() == role).collect(Collectors.toList());
+        return null;
     }
 
     /**
@@ -26,10 +25,10 @@ public class EmployeeList {
      * @return boolean if employeeList has employee with role
      */
     public boolean isEmployeeWithRolePresent(EmployeeRole role) {
-        return employeeList.stream().anyMatch(employee -> employee.getRole() == role);
+        return false;
     }
 
     public List<Employee> getEmployeeListByLimit(Integer limit) {
-        return employeeList.stream().limit(limit).collect(Collectors.toList());
+        return null;
     }
 }
